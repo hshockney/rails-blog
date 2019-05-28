fd647f87cae490f59b240b1b2cc7516472cfbf07
hshockney <shockney@atlanticbb.net>
2019-05-28 16:59:11 -0400

    added validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ApplicationRecord
     enum status1: { draft: 0, published: 1 }
     extend FriendlyId
     friendly_id :title, use: :slugged 
+    validates_presence_of :title, :body
 end
