c1c7887a159a0732582034bf71a9ee0639e4e671
hshockney <shockney@atlanticbb.net>
2019-05-31 13:23:18 -0400

    Integrated new gem for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
            content_tag(:p, greeting, class: "source_greeting")
         end
     end
+    
+    def copyright_generator
+        @copyright = HshockneyViewTool::Renderer.copyright 'Heather Shockney', 'All rights reserved'
+    end
 end
